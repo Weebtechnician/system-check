45ab8a32f6fb363b31a179d4a7441b6dab9e7dfd
Hans <hansmfigueroa@gmail.com>
2025-04-23 08:40:37 -0400

    Add alert system

--- monitor.py
+++ monitor.py
@@ -1,13 +1,20 @@
 import configparser
 import os
 import time
+import smtplib
+import datetime as dt
 
 # import psutil
 import subprocess
 import logging
 
+DEFAULT_TIMESTAMP = "1970-01-01 00:00:00"
+
 config = configparser.ConfigParser()
 config.read("config.ini")
+
+last_alert = configparser.ConfigParser()
+last_alert.read("last_alert.ini")
 
 logger = logging.getLogger("monitor")
 
@@ -22,6 +29,45 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
 )
+
+# --------------------- ALERTS ---------------------
+
+
+def send_alert(alert, subject):
+    with smtplib.SMTP(f"{config['email']['smtp']}", 587) as connection:
+        connection.starttls()
+        connection.login(
+            user=f"{config['email']['sender']}",
+            password=f"{config['email']['app_password']}",
+        )
+        connection.sendmail(
+            from_addr=config["email"]["sender"],
+            to_addrs=config["email"]["recipient"],
+            msg=f"Subject:{subject}\n\nThis is an automated message from your Pi monitor.\n\nPi has encountered an error:\n\n{alert}",
+        )
+
+
+def check_alerts(alert_type):
+    if "alerts" not in last_alert:
+        last_alert["alerts"] = {}
+    if alert_type not in last_alert["alerts"]:
+        last_alert["alerts"][alert_type] = DEFAULT_TIMESTAMP
+
+    last_time = dt.datetime.strptime(
+        last_alert["alerts"][alert_type], "%Y-%m-%d %H:%M:%S"
+    )
+    elapsed = (dt.datetime.now() - last_time).total_seconds()
+
+    if elapsed > float(config["alerts"]["threshold"]):
+        last_alert["alerts"][alert_type] = dt.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        with open("last_alert.ini", "w") as alert_file:
+            last_alert.write(alert_file)
+        return False
+    else:
+        return True
+
 
 # --------------------- NETWORK CHECK ---------------------
 
@@ -62,8 +108,18 @@ def check_connection():
         time.sleep(5 + retry_count * 2)
         retry_count += 1
     else:
-        logger.error("Max retries reached. Sending alert.")
+        alert_sent = check_alerts("network")
+
+        if alert_sent:
+            logger.info(
+                f"Alert previously sent on {last_alert['alerts']['network']}. Alert not resent."
+            )
+        else:
+            logger.error("Max retries reached. Sending alert.")
+            send_alert(result.stderr, "Pi network check failed.")
 
 
-# --------------------- SYSTEM ---------------------
+# --------------------- SYSTEM CHECKS ---------------------
+
+
 check_connection()
